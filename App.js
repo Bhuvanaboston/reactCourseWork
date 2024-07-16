0cd2898e048b41c64a371d98d9b5bf5a1005ab92
Bhuvanaboston <149010119+Bhuvanaboston@users.noreply.github.com>
2024-07-17 04:50:13 +0530

    Episode -03

--- App.js
+++ App.js
@@ -1,25 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const parent = React.createElement('div', { id: 'parent' }, [
-  React.createElement('div', { id: 'child1' }, [
-    React.createElement('h1', { id: 'grandchild1' }, 'Nested in react h1'),
-    React.createElement('h2', { id: 'grandchild2' }, 'Nested in react h2'),
-  ]),
-  React.createElement('div', { id: 'child2' }, [
-    React.createElement('h1', { id: 'grandchild3' }, 'Nested in react h1'),
-    React.createElement('h2', { id: 'grandchild4' }, 'Nested in react h2'),
-  ]),
-]);
+const JsxHeading = () => <h1>using JSX in App</h1>;
+const sampleElement = <h1>new React element</h1>;
 
-const heading1 = React.createElement(
-  'h1',
-  { id: 'sample' },
-  'Hello from React!!!'
-);
-console.log(heading1); //object
-const root1 = ReactDOM.createRoot(document.getElementById('mainContent'));
-root1.render(heading1);
-const root2 = ReactDOM.createRoot(document.getElementById('nextContent'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<JsxHeading />);
 
-root2.render(parent);
+const HeadingComponent = () => {
+  return (
+    <div>
+      {console.log('hello Bhuvana')}
+      {sampleElement}
+      <h1>Hello Bhuvana</h1>
+      {JsxHeading()}
+      <JsxHeading />
+      <JsxHeading></JsxHeading>
+      <h1>Hello boston</h1>
+    </div>
+  );
+};
+root.render(<HeadingComponent />);
