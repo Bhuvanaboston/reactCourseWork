92f2329643f9f874425f5856f51ac96fc1adb719
Bhuvanaboston <149010119+Bhuvanaboston@users.noreply.github.com>
2024-08-04 03:42:41 +0530

    Episode - 10

--- src/Body.js
+++ src/Body.js
@@ -20,15 +20,17 @@ const Body = () => {
     <Shimmer />
   ) : (
     <div className="body">
-      <div className="fiter">
-        <div className="search">
+      <div className="flex">
+        <div className="search m-4 p-4">
           <input
             type="text"
-            className="search-box"
+            className="border border-solid border-black m-4"
             value={searchText}
             onChange={(e) => setSearchText(e.target.value)}
           />
+
           <button
+            className="px-4 py-2 bg-green-100 rounded-lg"
             onClick={() => {
               setFilteredRestList(
                 restList.filter(
@@ -46,16 +48,21 @@ const Body = () => {
             Search
           </button>
         </div>
-        <button
-          className="fiter-btn"
-          onClick={() =>
-            setFilteredRestList(restList.filter((e) => e.info.avgRating > 4.0))
-          }
-        >
-          Top Rated Restaurants
-        </button>
+        <div className="m-4 p-4 flex items-center">
+          <button
+            className="px-4 py-2 bg-gray-100  rounded-lg"
+            onClick={() =>
+              setFilteredRestList(
+                restList.filter((e) => e.info.avgRating > 4.0)
+              )
+            }
+          >
+            Top Rated Restaurants
+          </button>
+        </div>
       </div>
-      <div className="res-container">
+
+      <div className="flex flex-wrap  m-4 shadow-lg bg-slate-100">
         {filteredRestList.map((card) => (
           <Link to={'/restaurentmenu/' + card.info.id} key={card.info.id}>
             <ResCard resData={card} />
--- src/ResCard.js
+++ src/ResCard.js
@@ -11,14 +11,17 @@ const ResCard = (props) => {
   } = props.resData?.info;
 
   return (
-    <div className="resCard">
-      <img className="food-image" src={CLOUD_URL + cloudinaryImageId}></img>
-      <div className="cardBody">
-        <h3>{name}</h3>
+    <div className="resCard m-4 p-4 w-[250px] h-[600px] bg-pink-50 rounded-lg shadow-lg hover:bg-red-900">
+      <img
+        className="h-[300px] rounded-lg"
+        src={CLOUD_URL + cloudinaryImageId}
+      ></img>
+      <div className="m-4 p-4">
+        <h3 className="font-bold text-lg">{name}</h3>
         <h4>{cuisines.join(', ')}</h4>
-        <h4>{avgRating}</h4>
+        <h4>{avgRating}⭐</h4>
         <h4>{slaString}</h4>
-        <h4>{costForTwo}</h4>
+        <h4 className="font-bold">{costForTwo}</h4>
       </div>
     </div>
   );
